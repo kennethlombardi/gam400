22ec28bf38baf22e7de1b4d1dc02d25e7461eb82
Kenneth Lombardi <kenneth.lombardi@digipen.edu>
2012-10-02 22:53:08 +0800

    The object factory changes and git practice.

--- projects/android/assets/lua/ObjectFactory.lua
+++ projects/android/assets/lua/ObjectFactory.lua
@@ -1,4 +1,17 @@
 local factory = {}
 
-local 
+-- the objects
+local objects = {}
+
+local function addObject(object)
+  
+end
+
+local function createObjectList( list )
+  list.addObject = addObject;
+end
+
+--decorate factory
+factory.addObject = addObject;
+
 return factory
