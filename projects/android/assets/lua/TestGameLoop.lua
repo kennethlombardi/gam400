48493b4cb58937beae8beb4e12a066c867487c8a
Kenneth Lombardi <kenneth.lombardi@digipen.edu>
2012-10-10 23:01:48 +0800

    New win32 and android host with MetaBall class

--- projects/android/assets/lua/TestGameLoop.lua
+++ projects/android/assets/lua/TestGameLoop.lua
@@ -1,5 +1,15 @@
 local done = false;
 
+local foo = MOAIFoo.new();
+foo:instanceHello();
+MOAIFooMgr.singletonHello();
+MOAIFoo.classHello();
+
+local metaBall = MOAIMetaBall.new();
+metaBall:instanceHello();
+MOAIMetaBall.classHello();
+MOAIMetaBallMgr.singletonHello();
+
 function testGameLoop ()
 	while not done do
 		coroutine.yield()
