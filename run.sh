7a791feccea96241f69e8b097e514ba6a8ed159a
Kenneth Lombardi <kenneth.lombardi@digipen.edu>
2012-10-30 11:21:38 +0800

    The sublime project folder (soon to have build systems in it as well as a project) and the modified run.sh to work with possible build system.

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-export MOAI_BIN=./hosts/win32
-export MOAI_CONFIG=./hosts/win32
-export MOAI_SOURCE=./source
-SOURCE=./source
-./$MOAI_BIN/moai.exe $MOAI_CONFIG/config.lua $MOAI_SOURCE/main.lua
- 
-echo "string"
+
+cd ./resources/lua/
+../../hosts/win32/moai.exe ../../hosts/win32/config.lua main.lua
