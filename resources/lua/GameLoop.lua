c5981939a96d53cb4a730891892cd161b4a444f0
Kenneth Lombardi <kenneth.lombardi@digipen.edu>
2012-11-03 00:46:27 +0800

    Resetting main game loop back to default. Hacks still exist in the ManagerGameLoop as test only. Game building begins in GameLoop.

--- resources/lua/GameLoop.lua
+++ resources/lua/GameLoop.lua
@@ -1,9 +1,6 @@
 local done = false;
 
-local Factory = require("Factory");
 function gameLoop ()
-	layer1 = Factory:createFromFile("Layer", "pickleFile.lua");
-	layer1:insertProp(Factory:create("Prop", {position = {x = 0, y = 0, z = 300}, name = "Prop", type = "Prop"}));
 	while not done do
 		coroutine.yield()
 
